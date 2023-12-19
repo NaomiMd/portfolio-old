7e1cde3b9fee961ba4e4cb4c8c46c2d53c6254d2
Naomi Maindret <naomi.maindret@hotmail.fr>
2023-12-19 19:21:57 +0100

    delete base

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: ' https://naomimd.github.io.',
-    base: '/portfolio',
+    site: ' https://naomimd.github.io/portfolio/',
 });
