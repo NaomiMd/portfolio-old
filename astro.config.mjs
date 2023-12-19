e3151e7cc7f43cb12f970a0c2487b6197488f77a
Naomi Maindret <naomi.maindret@hotmail.fr>
2023-12-19 19:29:05 +0100

    change link

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: ' https://naomimd.github.io/portfolio/',
+    site:'https://naomimd.github.io/portfolio/',
 });
